91a3ceb26696953d7211e29d34829a300eb4b92d
kazuma <saitoukazuma@saitoukazumanoiMac.local>
2020-05-08 23:37:50 +0900

    blogの生成を頑張った

---
+++ pages/blogs/_date/_slug/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <article class="content">
+      <h2>{{ title }}</h2>
+      <div>
+        <div v-for="tag in formatTags">
+          {{ tag }}
+        </div>
+        <time>{{ params.date }}</time>
+      </div>
+      <div v-html="bodyHtml"></div>
+  </article>
+</template>
+
+<script>
+import { sourceFileArray } from '../../../../contents/blogs/summary.json'
+export default {
+  components: {
+    
+  },
+  validate({ params }) {
+    return sourceFileArray.includes(`contents/blogs/md/${params.date}_${params.slug}.md`)
+  },
+  asyncData({ params }) {
+    return Object.assign({}, require(`~/contents/blogs/json/${params.date}_${params.slug}.json`), { params })
+  },
+  head() {
+    const title = this.title + " | kazuma-saitoh"
+    const url = `${this.url}blogs/${this.params.date}/${this.params.slug}/`
+    const desc = this.description || ''
+    return {
+      title: title,
+      meta: [
+        { hid: 'description', name: 'description', content: desc },
+        { hid: 'og:description', property: 'og:description', content: desc },
+        { hid: 'og:type', property: 'og:type', content: 'article' },
+        { hid: 'og:url', property: 'og:url', content: url },
+        { hid: 'og:title', property: 'og:title', content: title }
+      ],
+      link: [{ rel: 'canonical', href: url }]
+    }
+  },
+  computed: {
+    formatTags() {
+      return this.tags.split(',')
+    },
+    shareTitle() {
+      return this.title + " | kazuma-saito"
+    }
+  }
+}
+</script>
--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -1,38 +1,15 @@
 <template>
   <main>
     <h1>blogs</h1>
-    <article class="section section_top">
-      <Message />
-    </article>
-    <article class="section section_back-green">
-      <Profile />
-    </article>
+
   </main>
 </template>
 
 <script>
-import Message from '~/components/Message.vue';
-import Profile from '~/components/Profile.vue';
-
 export default {
   components: {
-    Message,
-    Profile
   }
 }
 </script>
 <style lang="scss" scoped>
-.section {
-  padding: 2rem 1rem;
-  color: #1c4220;
-
-  &_top {
-    padding-bottom: 0;
-  }
-
-  &_back-green {
-    background-color: #1c4220;
-    color: #fff;
-  }
-}
 </style>
