277e3eeb7b7ad6e8d6200753d75cb7966777a7dc
kazuma-saitoh <kazuma-saitoh@cybozu.co.jp>
2020-05-25 01:44:18 +0900

    test

--- plugins/routerOptions.js
+++ plugins/routerOptions.js
@@ -1,9 +1,9 @@
-// export default async ({ app }) => {
-//   app.router.afterEach(() => {
-//     let navClass = document.querySelector(".btn-hamburger").classList
-//     if (navClass.indexOf("active") >= 0) {
-//       document.querySelector(".btn-hamburger").classList.remove("active")
-//       document.querySelector("nav").classList.remove("active")
-//     }
-//   })
-// }
+export default async ({ app }) => {
+  app.router.afterEach(() => {
+    let navClass = document.querySelector(".btn-hamburger").classList
+    if (navClass.indexOf("active") >= 0) {
+      document.querySelector(".btn-hamburger").classList.remove("active")
+      document.querySelector("nav").classList.remove("active")
+    }
+  })
+}
